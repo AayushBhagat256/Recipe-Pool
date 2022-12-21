32aa8b4f98aae794a8b28f20659b99c327e7bc71
AayushBhagat256 <113020296+AayushBhagat256@users.noreply.github.com>
2022-12-21 12:52:02 +0530

    API integrated

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Homepage.css'
 //import Cards from './Card'
 //import * as React from 'react';
@@ -12,7 +12,7 @@ import Collapse from '@mui/material/Collapse';
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { blue, grey, red } from '@mui/material/colors';
+import {  grey, red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
@@ -20,7 +20,7 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-import photo from './21547.jpg'
+//import photo from './21547.jpg'
 import axios from 'axios';
 
 const ExpandMore = styled((props) => {
@@ -56,11 +56,16 @@ function Homepage() {
 
 
   }
+  const [entry,setEntry]=useState([])
+  useEffect(() => {
+      getData();
+    }, [])
+    let linkurl = "https://therecipepool.pythonanywhere.com/"
   const getData=()=>{
     //var axios = require('axios');
 var data = JSON.stringify({
   "meal": [
-    "dinner"
+    "breakfast"
   ]
 });
 
@@ -79,9 +84,11 @@ axios(config)
   console.log(JSON.stringify(response.data));
   console.log(response.data[0])
   console.log(response.data[0].id)
+  setEntry(response.data)
   // let k = response.data.length()
   // console.log(k);
 })
+//.then((objdata)=>{setEntry(objdata.data)})
 .catch(function (error) {
   console.log(error);
 });
@@ -92,87 +99,112 @@ axios(config)
       getData();
     },[]
   )
+//   var i;
+//  const items= [];
+//  function getIngridents(){} ;
+//  var m;
+//   // const getIngridents=()=>{
+//   //   const items= [];
+//   //   for( i=0;i<=map.ingredient_list.length;i++){
+//   //     items.push(<p>{map.ingredient_list[i].name}</p>)
+//   //   }
+//   //   return <p>{items}</p>
+//   // }
+//   const {getIngridents}=()=>{
+                              
+//     for( i=0;i<=map.ingredient_list.length;i++){
+//     items.push(map.ingredient_list[i].name)
+//     }
+//     return <p>{items}</p>
+//   }
   return (
     <div className='mainbody'>
       <div className="welcome">Welcome , {username}</div>
       {/* lets make a grid here */}
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={3} >
-          <Grid item xs={6} md={3} >
-            <Item >
-              <div className="card mx-1">
-                <Card sx={{ maxWidth: 345, bgcolor: grey[900] }}>
-                  <CardHeader sx={{ color: 'white' }}
-                    avatar={
-                      <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-
-                      </Avatar>
-                    }
-                    action={
-                      <IconButton aria-label="settings" sx={{ color: 'white' }} onClick={settingfun}>
-                        <MoreVertIcon />
-                      </IconButton>
-                    }
-                    title="Title here"
-                    subheaderTypographyProps={{ color: 'white' }}
-                    subheader="Time Stamp here"
-
-                  />
-
-                  <CardMedia
-                    component="img"
-                    height="194"
-                    image={photo}
-                    alt="Paella dish"
-                  />
-                  <CardContent>
-                    <Typography variant="body2" color="text.secondary" className='des' sx={{ color: 'white' }}>
-                      <span className='des'>Description here:</span>
-                      <p className='des'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto enim atque excepturi veniam voluptatibus maiores cumque, deserunt obcaecati vero reiciendis quia recusandae impedit repellat officia? Iusto assumenda nulla voluptatum ipsum iste tenetur optio dolorum?</p>
-                    </Typography>
-                  </CardContent>
-                  <CardActions disableSpacing>
-                    <IconButton aria-label="add to favorites" sx={{ color: 'white' }} className='like'>
-                      <FavoriteIcon />
-                    </IconButton>
-                    <IconButton aria-label="share" sx={{ color: 'white' }} className='share'>
-                      <ShareIcon />
-                    </IconButton>
-                    <ExpandMore
-                      expand={expanded}
-                      onClick={handleExpandClick}
-                      aria-expanded={expanded}
-                      aria-label="show more"
-                      sx={{ color: 'white' }}
-                      className='showmore'
-                    >
-                      <ExpandMoreIcon />
-                    </ExpandMore>
-                  </CardActions>
-                  <Collapse in={expanded} timeout="auto" unmountOnExit>
+          {
+            entry.map(map=>
+              <Grid item xs={6} md={3} >
+              <Item >
+                <div className="card mx-1">
+                  <Card sx={{ maxWidth: 345, bgcolor: grey[900] }}>
+                    <CardHeader sx={{ color: 'white' }}
+                      avatar={
+                        <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+  
+                        </Avatar>
+                      }
+                      action={
+                        <IconButton aria-label="settings" sx={{ color: 'white' }} onClick={settingfun}>
+                          <MoreVertIcon />
+                        </IconButton>
+                      }
+                      title={map.cuisine.cuisine_name}
+                      subheaderTypographyProps={{ color: 'white' }}
+                      subheader={map.label}
+  
+                    />
+  
+                    <CardMedia
+                      component="img"
+                      height="194"
+                      //image={photo}
+                      image={linkurl+map.image}
+                      alt="Paella dish"
+                    />
                     <CardContent>
-                      <Typography paragraph sx={{ color: 'white' }}>Method:</Typography>
-                      {/* <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
-          </Typography> */}
-                      <Typography paragraph sx={{ color: 'white' }}>
-                        Health label :
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima quo recusandae, labore tenetur quae sint.</p>
-                      </Typography>
-                      <Typography paragraph sx={{ color: 'white' }}>
-                        Total Nutrients:
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt, nam odio quia, vel consectetur enim debitis quos explicabo inventore ratione necessitatibus in ea!</p>
-                      </Typography>
-                      <Typography sx={{ color: 'white' }}>
-                        Set aside off of the heat to let rest for 10 minutes, and then serve.
+                      <Typography variant="body2" color="text.secondary" className='des' sx={{ color: 'white' }}>
+                        <span className='des'>Why its healthy ??</span>
+                        <p className='des'>{map.healthLabels}</p>
                       </Typography>
                     </CardContent>
-                  </Collapse>
-                </Card>
-              </div>
-            </Item>
-          </Grid>
+                    <CardActions disableSpacing>
+                      <IconButton aria-label="add to favorites" sx={{ color: 'white' }} className='like'>
+                        <FavoriteIcon />
+                      </IconButton>
+                      <IconButton aria-label="share" sx={{ color: 'white' }} className='share'>
+                        <ShareIcon />
+                      </IconButton>
+                      <ExpandMore
+                        expand={expanded}
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                        sx={{ color: 'white' }}
+                        className='showmore'
+                      >
+                        <ExpandMoreIcon />
+                      </ExpandMore>
+                    </CardActions>
+                    <Collapse in={expanded} timeout="auto" unmountOnExit>
+                      <CardContent>
+                        <Typography paragraph sx={{ color: 'white' }}>Method:</Typography>
+                        {/* <Typography paragraph>
+              Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
+              aside for 10 minutes.
+            </Typography> */}
+                        <Typography paragraph sx={{ color: 'white' }}>
+                          Health label :
+                          
+                        {/* <p>{map.ingredient_list.name}</p> */}
+                         {/* <p>{getIngridents()}</p>  */}
+                        </Typography>
+                        <Typography paragraph sx={{ color: 'white' }}>
+                          Total Nutrients:
+                          <p>{map.totalNutrients}</p>
+                        </Typography>
+                        <Typography sx={{ color: 'white' }}>
+                          Set aside off of the heat to let rest for 10 minutes, and then serve.
+                        </Typography>
+                      </CardContent>
+                    </Collapse>
+                  </Card>
+                </div>
+              </Item>
+            </Grid>  
+            )
+          }
           {/* <Grid item xs={6} md={4}>
           <Item>xs=6 md=4</Item>
         </Grid>
@@ -182,148 +214,9 @@ axios(config)
         <Grid item xs={6} md={8}>
           <Item>xs=6 md=8</Item>
         </Grid> */}
-          <Grid item xs={6} md={3}>
-            <Item>
-              <div className="card mx-1">
-                <Card sx={{ maxWidth: 345 }}>
-                  <CardHeader
-                    avatar={
-                      <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-
-                      </Avatar>
-                    }
-                    action={
-                      <IconButton aria-label="settings">
-                        <MoreVertIcon />
-                      </IconButton>
-                    }
-                    title="Title here"
-                    subheader="Time Stamp here"
-                  />
-                  <CardMedia
-                    component="img"
-                    height="194"
-                    image={photo}
-                    alt="Paella dish"
-                  />
-                  <CardContent>
-                    <Typography variant="body2" color="text.secondary">
-                      Description here:
-                      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto enim atque excepturi veniam voluptatibus maiores cumque, deserunt obcaecati vero reiciendis quia recusandae impedit repellat officia? Iusto assumenda nulla voluptatum ipsum iste tenetur optio dolorum?</p>
-                    </Typography>
-                  </CardContent>
-                  <CardActions disableSpacing>
-                    <IconButton aria-label="add to favorites">
-                      <FavoriteIcon />
-                    </IconButton>
-                    <IconButton aria-label="share">
-                      <ShareIcon />
-                    </IconButton>
-                    <ExpandMore
-                      expand={expanded}
-                      onClick={handleExpandClick}
-                      aria-expanded={expanded}
-                      aria-label="show more"
-                    >
-                      <ExpandMoreIcon />
-                    </ExpandMore>
-                  </CardActions>
-                  <Collapse in={expanded} timeout="auto" unmountOnExit>
-                    <CardContent>
-                      <Typography paragraph>Method:</Typography>
-                      {/* <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
-          </Typography> */}
-                      <Typography paragraph>
-                        Health label :
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima quo recusandae, labore tenetur quae sint.</p>
-                      </Typography>
-                      <Typography paragraph>
-                        Total Nutrients:
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt, nam odio quia, vel consectetur enim debitis quos explicabo inventore ratione necessitatibus in ea!</p>
-                      </Typography>
-                      <Typography>
-                        Set aside off of the heat to let rest for 10 minutes, and then serve.
-                      </Typography>
-                    </CardContent>
-                  </Collapse>
-                </Card>
-              </div>
-            </Item>
-          </Grid>
+          
           {/* #one more */}
-          <Grid item xs={6} md={3}>
-            <Item>
-              <div className="card mx-1">
-                <Card sx={{ maxWidth: 345 }}>
-                  <CardHeader
-                    avatar={
-                      <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-
-                      </Avatar>
-                    }
-                    action={
-                      <IconButton aria-label="settings">
-                        <MoreVertIcon />
-                      </IconButton>
-                    }
-                    title="Title here"
-                    subheader="Time Stamp here"
-                  />
-                  <CardMedia
-                    component="img"
-                    height="194"
-                    image={photo}
-                    alt="Paella dish"
-                  />
-                  <CardContent>
-                    <Typography variant="body2" color="text.secondary">
-                      Description here:
-                      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto enim atque excepturi veniam voluptatibus maiores cumque, deserunt obcaecati vero reiciendis quia recusandae impedit repellat officia? Iusto assumenda nulla voluptatum ipsum iste tenetur optio dolorum?</p>
-                    </Typography>
-                  </CardContent>
-                  <CardActions disableSpacing>
-                    <IconButton aria-label="add to favorites">
-                      <FavoriteIcon />
-                    </IconButton>
-                    <IconButton aria-label="share">
-                      <ShareIcon />
-                    </IconButton>
-                    <ExpandMore
-                      expand={expanded}
-                      onClick={handleExpandClick}
-                      aria-expanded={expanded}
-                      aria-label="show more"
-                    >
-                      <ExpandMoreIcon />
-                    </ExpandMore>
-                  </CardActions>
-                  <Collapse in={expanded} timeout="auto" unmountOnExit>
-                    <CardContent>
-                      <Typography paragraph>Method:</Typography>
-                      {/* <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
-          </Typography> */}
-                      <Typography paragraph>
-                        Health label :
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima quo recusandae, labore tenetur quae sint.</p>
-                      </Typography>
-                      <Typography paragraph>
-                        Total Nutrients:
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt, nam odio quia, vel consectetur enim debitis quos explicabo inventore ratione necessitatibus in ea!</p>
-                      </Typography>
-                      <Typography>
-                        Set aside off of the heat to let rest for 10 minutes, and then serve.
-                      </Typography>
-                    </CardContent>
-                  </Collapse>
-                </Card>
-              </div>
-            </Item>
           </Grid>
-        </Grid>
       </Box>
 
     </div>
